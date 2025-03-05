e67737fd7f65926c8718662e98de5394356439c5
23Yadnesh <yrane555@gmail.com>
2025-03-05 16:24:09 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+ "use strict";// treat all js code as a newer version
+
+ console.log(3
+    +3 )// Code readablity shoulde be high
+
+ console.log("yadnesh")
+
+ let name = "Yadnesh"
+ let age = 24
+ let isloggedin = false
+
+//  number => 2 to powerd 53
+// bigint // special used when the number is high example stocks,trading
+// string => "" // try to use in double cort
+// boolean => true/false
+//null => standalone value its a special type or we can say its a object
+//undefine
+// symbol => unique // used to find uniquness
+
+// object
+
+console.log(typeof undefine); // undefine
+console.log (typeof null); // object
