2207c465dea0a05058628f57a3c9dcbcc36c5e44
23Yadnesh <yrane555@gmail.com>
2025-03-07 16:58:08 +0000

    conversion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+let valueInNumber = Number (score)
+
+// console.log (valueInNumber);
+// console.log(typeof valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=>0
+
+let isloggedIn = 0
+
+let boolenvalue = Boolean (isloggedIn)
+
+// console.log (boolenvalue);
+
+// true => 1; false=>0
+// "" => false
+// "yadnesh" => true
+
+let someNumber = 33
+
+let stringValue = String (someNumber)
+
+console.log(stringValue);
+console.log( typeof stringValue);
