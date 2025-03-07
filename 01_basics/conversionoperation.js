abbc85eef03b0d44bd3f5cb135082dfc13801f59
23Yadnesh <yrane555@gmail.com>
2025-03-07 17:43:39 +0000

    operations

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -24,5 +24,42 @@ let someNumber = 33
 
 let stringValue = String (someNumber)
 
-console.log(stringValue);
-console.log( typeof stringValue);
+// console.log(stringValue);
+// console.log( typeof stringValue);
+
+// *****OPERATIONS*****
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = hello
+let str2 = yadnesh
+let str3 = let1 + let2
+
+// console.log (str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console .log ((3+ 4) - 5 % 7);  
+
+// console.log (+true);
+// console.log (+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log (gameCounter);
+
+// study prefix and postfix from js mdn documentation
+
+
