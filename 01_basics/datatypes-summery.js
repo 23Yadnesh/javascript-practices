309cf0cb204c06956fc3e2f75b89c87ecb2ebcb8
23Yadnesh <yrane555@gmail.com>
2025-05-11 16:36:13 +0000

    datatype&summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,33 @@
+// primitive & non-primitive
+
+// primitive = number, string, boolearn, null, undefine, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const loggedIn = false
+const outsideTemp = null
+let useremail;
+ 
+const id = symbol ('123')
+const anotherId = symbol ('123')
+
+console.log(id===anotherId);
+
+// non primitive = arrey, Object, functions
+
+// arrey is writen in square bracket  const hero = [ ironman, spiderman, hulk]
+
+// object is writen in curly bracket 
+// let myobj = { name : "yadnesh", 
+// age : 24, }
+
+// Way of writing function  
+// Const myFunction = function (){ 
+//  console.log ( "hello yadnesh"); }
+
+
+
+
+// there are two types of memory storing process Stack and Heap
+// stack is used by primitive type & Heap is used by non primitive type.
