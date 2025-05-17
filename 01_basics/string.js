562dab67a17fff279412f73f6a3560b04c535570
23Yadnesh <yrane555@gmail.com>
2025-05-17 21:49:38 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,4 +1,16 @@
 const name = "yadnesh"
 const repo = 20
 
-console.log(name + repo);
+// console.log(name + repo);
+
+console.log(`Hello my name is ${name} and i have repo count ${repo}`);
+
+const gameName = new String ('yadnesh')
+
+console.log(gameName[4]);
+console.log(gameName.length);
+console.log(gameName.charAt(5));
+
+
+
+
