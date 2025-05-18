2d07133a4211bda039e18581b6fced1fa102a02f
23Yadnesh <yrane555@gmail.com>
2025-05-18 11:29:15 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -10,6 +10,21 @@ const gameName = new String ('yadnesh')
 console.log(gameName[4]);
 console.log(gameName.length);
 console.log(gameName.charAt(5));
+// Prototypes
+
+const newString = gameName.substring(0,5)
+console.log (newString);
+
+const anotherString = gameName.slice(-7,3)
+console.log(anotherString);
+
+const myName = "  yadnesh  "
+console.log(myName);
+console.log(myName.trim( ));
+
+const newUrl = "https;//yadnesh.com/home20%page"
+console.log(newUrl.replace('20%', '-'));
+
 
 
 
