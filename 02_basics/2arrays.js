1d06fc1456a398e6d59ad2ca28685fcd463b2fba
23Yadnesh <yrane555@gmail.com>
2025-05-19 15:59:10 +0000

    HEROS

--- 02_basics/2arrays.js
+++ 02_basics/2arrays.js
@@ -2,3 +2,19 @@ const marvelHeros = [spiderman, Thor , Hulk]
 const dcHeros = [batman, flash, superman]
 
 marvelHeros.push(dcHeros)
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[3][1]);
+
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+// spread
+
+const allNewHeros = [...dcHeros, ...marvelHeros]
+// console.log(allNewHeros);
+
+
+const another_arry = [1, 2, 3, 4 [5, 6, 7 ] 8, 9  [1, 2 [3, 4]] ]
+
+//console.log(another_arry);
