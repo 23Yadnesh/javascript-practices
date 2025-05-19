8618b979f751fad4ac9b81c08cb420998030a628
23Yadnesh <yrane555@gmail.com>
2025-05-19 18:51:01 +0000

    array complete

--- 02_basics/2arrays.js
+++ 02_basics/2arrays.js
@@ -1,5 +1,5 @@
-const marvelHeros = [spiderman, Thor , Hulk]
-const dcHeros = [batman, flash, superman]
+const marvelHeros = ["spiderman", "Thor", "Hulk"]
+const dcHeros = ["batman", "flash", "superman"]
 
 marvelHeros.push(dcHeros)
 
@@ -15,6 +15,19 @@ const allNewHeros = [...dcHeros, ...marvelHeros]
 // console.log(allNewHeros);
 
 
-const another_arry = [1, 2, 3, 4 [5, 6, 7 ] 8, 9  [1, 2 [3, 4]] ]
+const another_arry = [1, 2, 3, 4, [5, 6, 7 ] , 8, 9,  [1, 2, [3, 4]] ]
+const newarry = another_arry.flat(Infinity);
 
-//console.log(another_arry);
+// console.log(newarry);
+
+console.log(Array.isArray("yadnesh"));
+console.log(Array.from("yadnesh"));
+console.log(Array.from({name:"yadnesh"})); //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
