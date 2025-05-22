a057f436b077041f1045ba03edfa2f6e227448d7
23Yadnesh <yrane555@gmail.com>
2025-05-22 15:58:50 +0000

    objects basics 1

--- 02_basics/3objects.js
+++ 02_basics/3objects.js
@@ -4,7 +4,7 @@
 const jsUser = {
     name: "Yadnesh",
     "full name": "Yadnesh Rane",
-   [mySym]: "key1",
+  //[mySym]: "key1", i've done something wrong here.
     email: "yrane555@gmail.com",
     age: 24,
     location: "Bhandup",
@@ -13,10 +13,29 @@ const jsUser = {
 
 }
 
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["full name"]);
-console.log(jsUser[mySym]);
+//console.log(jsUser.email);
+//console.log(jsUser["email"]);
+//console.log(jsUser["full name"]);
+//console.log(jsUser[mySym]);
 
+jsUser.email= "yadnesh2304@gmail.com"
+//Object.freeze (jsUser)
+jsUser.email = "xyz@gmail.com"
+
+//console.log(jsUser);
+
+jsUser.greeting = function() {
+    console.log(`Hello my name `);
+    }
+
+
+jsUser.greetingTwo = function() {
+    console.log(`Hello my name is, ${this.name}`);
+    }
+   // console.log(jsUser.greeting());
+    //console.log(jsUser.greetingTwo());
+
+    // when ever you access values most of the time you use dots(.) some times we use Bracket [] if there is no other option.   
+    
 
 
