c7c7ff3fb36ef0eef680269b333f92fdc16f31b3
23Yadnesh <yrane555@gmail.com>
2025-05-22 16:50:53 +0000

    nesting

--- 02_basics/4objects.js
+++ 02_basics/4objects.js
@@ -1,3 +1,21 @@
-const tinderUser = new object()
+//const tinderUser = new object() this is single term object 
+// const tinderUser = {} this is non single term object
+const tinderUser ={}
 
-console.log(tinderUser);
+tinderUser.id = "4323"
+tinderUser.name = "yadnesh"
+tinderUser.loggedIn = "false"
+
+//console.log(tinderUser);
+
+const instaUser = {
+    email:"yrane555@gmail.com",
+    userName: {
+        fullName:{
+            firstName: "Yadnesh",
+            lastName: "Rane",
+        }
+    }
+}
+
+console.log(instaUser.userName.fullName.lastName);
