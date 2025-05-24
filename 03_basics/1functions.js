38d43e3c6988f767b5b39d33ae98036bf767a5e4
23Yadnesh <yrane555@gmail.com>
2025-05-24 14:57:15 +0000

    functions

--- 03_basics/1functions.js
+++ 03_basics/1functions.js
@@ -1,13 +1,44 @@
-function myName(params) {
-console.log("Y");
-console.log("a");
-console.log("d");
-console.log("n");
-console.log("e");
-console.log("s");
-console.log("h");
+
+// function myName() {
+// console.log("Y");
+// console.log("a");
+// console.log("d");
+// console.log("n");
+// console.log("e");
+// console.log("s");
+// console.log("h");
 
     
+// }
+
+// myName()
+
+// function addTwoNumbers(number1, number2 ) {
+//    console.log(number1 + number2);
+    
+    
+// }
+
+// const result = addTwoNumbers( 3, 6 )
+
+// console.log("Result:",result);
+
+function addTwoNumbers(number1, number2 ) {
+   
+//    let result = number1 + number2
+//    return result
+    return number1+number2
 }
 
-myName()
+const result = addTwoNumbers( 3, 6 )
+
+// console.log("Result:",result);
+
+function loggeInUser (myName) {
+    return`${"myName"} just logged in`
+}
+
+console.log (loggeInUser("Yadnesh"))
+console.log(loggeInUser(""));
+
+
