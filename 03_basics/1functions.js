37993eeb8afccf8b713cbe7e45d266ea1c45c306
23Yadnesh <yrane555@gmail.com>
2025-05-24 14:24:04 +0000

    funtions

---
+++ 03_basics/1functions.js
@@ -0,0 +1,13 @@
+function myName(params) {
+console.log("Y");
+console.log("a");
+console.log("d");
+console.log("n");
+console.log("e");
+console.log("s");
+console.log("h");
+
+    
+}
+
+myName()
