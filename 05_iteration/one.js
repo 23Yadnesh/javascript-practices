4958c01b07b0a92e3abff5e7c7f7d40dc11e260f
23Yadnesh <yrane555@gmail.com>
2025-05-26 22:10:22 +0000

    for loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,65 @@
+// // for 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+    
+// }
+
+// for (let i = 0; i<=10;  i++ ){
+//     const element = i;
+//     if (element === 5){
+//         console.log("5 is the best ");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//  console.log(`outer loop: ${i}`);
+ 
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(i + '*' + j + '=' + i*j);
+        
+        
+//     }
+    
+// }
+
+// let myArray = ["superman", "flash", "batmen"]
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+// break and Continue
+
+// for (let i = 0; i <=10; i++) {
+//     if (i===5) {
+//         console.log("detected 5");
+//         break
+        
+//     }
+//     console.log(`value of i is :${i}`);
+    
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     if (i===5) {
+//         console.log("detected 5");
+//         continue
+//     }
+//     console.log(`value of i is :${i}`);
+    
+// }
