b4e4b957119f00c6c4bc62e150ad1d9503f2874d
23Yadnesh <yrane555@gmail.com>
2025-05-26 19:22:56 +0000

    basic

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -1,12 +1,24 @@
 // if
+// const isLoggedIn = true
+// const temprature = 41
 
-const isLoggedIn = true
-const temprature = 41
+// if (temprature > 41){
+//     console.log("Temprature is less than 50");
+    
+// }else {
+//     console.log("Tempreture is more than 50");
+    
+// }
+// <, >, <=, >=, ==, !=, ===, !==
 
-if (temprature > 41){
-    console.log("Temprature is less than 50");
+// const score = 200
+
+// if (score < 300) {
+//     const power = "fly"
+//     console.log(`use power: ${power}`);
     
-}else {
-    console.log("Tempreture is more than 50");
-    
-}
+// }
+
+const balance = 1000
+
+// if (balance > 500 ) console.log("test"), console.log("test2"); never right code like this. not good practice for industry
