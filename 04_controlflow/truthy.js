4c46ed665e4666224bedb26b42f123988c2a197d
23Yadnesh <yrane555@gmail.com>
2025-05-26 20:56:50 +0000

    Nullish & Terniary operators

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -18,9 +18,26 @@ if (userEmail) {
     
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("object is empty");
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
     
-}
+// }
+
+// Nullish coalescing operator (??): null undefined
+ 
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 5 ?? 10
+
+// console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+
+const coffee = 150
+coffee >= 200 ? console.log("yes") : console.log("No");
+
