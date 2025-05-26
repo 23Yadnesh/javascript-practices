6b6de89538894ce417836afe8b8e3471b5613480
23Yadnesh <yrane555@gmail.com>
2025-05-26 19:10:30 +0000

    control flow basic

---
+++ 04_controlflow/one.js
@@ -0,0 +1,12 @@
+// if
+
+const isLoggedIn = true
+const temprature = 41
+
+if (temprature > 41){
+    console.log("Temprature is less than 50");
+    
+}else {
+    console.log("Tempreture is more than 50");
+    
+}
