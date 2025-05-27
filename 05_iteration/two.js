959c9e830de8076ef3b6af1a8d18c7fae00c2dd7
23Yadnesh <yrane555@gmail.com>
2025-05-27 19:23:22 +0000

    while & dowhile loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,33 @@
+// *********While Loop
+// // let index = 0
+// // while (index <= 10) {
+// //     console.log(`value is less than ${index}`);
+// //     index = index + 2
+    
+// // }
+
+// let heros = ["superman", "Batman", "spiderman" ]
+// let arr = 0
+
+// while (arr < heros.length) {
+//     console.log(`our superheros are ${heros[arr]}`); 
+//     arr = arr + 1
+// }
+
+// ******* Dowhile Loop
+
+// let score = 0
+
+// do {
+// console.log(`score is ${score}`);
+// score++
+    
+// } while (score <= 10);
+
+let score = 11
+
+do {
+console.log(`score is ${score}`);
+score++
+    
+} while (score <= 10);
