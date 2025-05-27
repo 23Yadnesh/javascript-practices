9aa3c2c1fba6d3cd74c6f416fe3c39cdc8ea28e6
23Yadnesh <yrane555@gmail.com>
2025-05-27 20:50:47 +0000

    forin loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,20 @@
+// *********** forin
+
+// const myObj = {
+//     js : " Javascript",
+//     cpp : "c++",
+//     Rb : "rubi"
+    
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} stands for ${myObj[key]}`);
+       
+//     }
+
+const programming = ["js", "cpp", "rb"]
+
+for (const val in programming) {
+console.log(programming[val]);
+
+    }
