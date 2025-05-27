da4c336cb01db522f6e0e28c88ee7eeeeba4e09d
23Yadnesh <yrane555@gmail.com>
2025-05-27 20:32:23 +0000

    forof loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,30 @@
+//************ forof Loop
+
+const num = [1, 2, 3, 4, 5]
+
+for (const key of num) {
+    // console.log(key);
+    
+}
+
+const greeting = "Hello World"
+
+for (const val of greeting) {
+    // console.log(`define each charachter ${val}`);
+    
+}
+
+// *********maps
+
+const map = new Map
+
+map.set ( 'IND', "India")
+map.set ( 'FR', "France")
+map.set ('RS', "Rusia")
+
+// console.log(map);
+ 
+for (const [key, Value] of map) {
+    console.log(key,':-', Value);
+     
+}
